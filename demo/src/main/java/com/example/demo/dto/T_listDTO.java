038c4d94e8893673de15954fa62525aed21fffde
Kim DongJin <kxv1234@naver.com>
2020-01-17 12:58:11 +0900

    Lombok Library 사용

--- demo/src/main/java/com/example/demo/dto/T_listDTO.java
+++ demo/src/main/java/com/example/demo/dto/T_listDTO.java
@@ -1,31 +1,40 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class T_listDTO {
 	
 	public int SEQ;
 	public String ITEMID;
 	
-	public T_listDTO() {
-		
-	}
-	public T_listDTO(int sEQ, String iTEMID) {
-		super();
-		SEQ = sEQ;
-		ITEMID = iTEMID;
-	}
-	
-	public int getSEQ() {
-		return SEQ;
-	}
-	public void setSEQ(int sEQ) {
-		SEQ = sEQ;
-	}
-	public String getITEMID() {
-		return ITEMID;
-	}
-	public void setITEMID(String iTEMID) {
-		ITEMID = iTEMID;
-	}
+//	public T_listDTO() {
+//		
+//	}
+//	public T_listDTO(int sEQ, String iTEMID) {
+//		super();
+//		SEQ = sEQ;
+//		ITEMID = iTEMID;
+//	}
+//	
+//	public int getSEQ() {
+//		return SEQ;
+//	}
+//	public void setSEQ(int sEQ) {
+//		SEQ = sEQ;
+//	}
+//	public String getITEMID() {
+//		return ITEMID;
+//	}
+//	public void setITEMID(String iTEMID) {
+//		ITEMID = iTEMID;
+//	}
 	
 	
 }
